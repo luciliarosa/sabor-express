352153c43f5bdb19aed2a77f26e36d8ef72affc8
luciliarosa <lucilia.passos.rosa@outlook.com>
2025-11-04 10:31:36 -0300

    feat: add custom menu

--- app.py
+++ app.py
@@ -1,4 +1,7 @@
-print('Sabor Express\n')
+print("""
+█▀ ▄▀█ █▄▄ █▀█ █▀█   █▀▀ ▀▄▀ █▀█ █▀█ █▀▀ █▀ █▀
+▄█ █▀█ █▄█ █▄█ █▀▄   ██▄ █░█ █▀▀ █▀▄ ██▄ ▄█ ▄█
+""")
 
 print ('1. Cadastrar restaurante')
 print ('2. Listar restaurante')
@@ -6,5 +9,4 @@ print ('3. Ativar restaurante')
 print ('4. Sair\n')
 
 opcao_escolhida = input('Escolha uma opção: ')
-print (f'Você escolheu a opção {opcao_escolhida}')
-
+print (f'Você escolheu a opção: {opcao_escolhida}')
