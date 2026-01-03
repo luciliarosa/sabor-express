5385fcf999cfcb3be16fb37cae29958793af1ebd
Lucilia Rosa <lucilia.passos.rosa@outlook.com>
2026-01-03 20:38:48 -0300

    feat: add option to exit the menu

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+import os
+
 print("""
 █▀ ▄▀█ █▄▄ █▀█ █▀█   █▀▀ ▀▄▀ █▀█ █▀█ █▀▀ █▀ █▀
 ▄█ █▀█ █▄█ █▄█ █▀▄   ██▄ █░█ █▀▀ █▀▄ ██▄ ▄█ ▄█
@@ -8,5 +10,18 @@ print ('2. Listar restaurante')
 print ('3. Ativar restaurante')
 print ('4. Sair\n')
 
-opcao_escolhida = input('Escolha uma opção: ')
-print (f'Você escolheu a opção: {opcao_escolhida}')
+opcao_escolhida = int(input('Escolha uma opção: '))
+
+#função
+def finalizar_app():
+  os.system('cls')
+  print('Finalizando APP\n')
+
+if opcao_escolhida == 1:
+  print('Cadastrar restaurantes')
+elif opcao_escolhida == 2:
+  print('Listar restaurantes')
+elif opcao_escolhida == 3:
+  print('Ativar restaurantes')
+else:
+  finalizar_app()
