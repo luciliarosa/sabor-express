915331173ecdff35fae4bcca9522b99b4c80fd82
Lucilia Rosa <lucilia.passos.rosa@outlook.com>
2026-01-03 21:34:08 -0300

    refactor: organize code into functions

--- app.py
+++ app.py
@@ -1,27 +1,36 @@
 import os
 
-print("""
-█▀ ▄▀█ █▄▄ █▀█ █▀█   █▀▀ ▀▄▀ █▀█ █▀█ █▀▀ █▀ █▀
-▄█ █▀█ █▄█ █▄█ █▀▄   ██▄ █░█ █▀▀ █▀▄ ██▄ ▄█ ▄█
-""")
+def exibir_nome_do_programa():
+  print("""
+  █▀ ▄▀█ █▄▄ █▀█ █▀█   █▀▀ ▀▄▀ █▀█ █▀█ █▀▀ █▀ █▀
+  ▄█ █▀█ █▄█ █▄█ █▀▄   ██▄ █░█ █▀▀ █▀▄ ██▄ ▄█ ▄█
+  """)
 
-print ('1. Cadastrar restaurante')
-print ('2. Listar restaurante')
-print ('3. Ativar restaurante')
-print ('4. Sair\n')
+def exibir_opcoes():
+  print ('1. Cadastrar restaurante')
+  print ('2. Listar restaurante')
+  print ('3. Ativar restaurante')
+  print ('4. Sair\n')
 
-opcao_escolhida = int(input('Escolha uma opção: '))
-
-#função
 def finalizar_app():
   os.system('cls')
   print('Finalizando APP\n')
 
-if opcao_escolhida == 1:
-  print('Cadastrar restaurantes')
-elif opcao_escolhida == 2:
-  print('Listar restaurantes')
-elif opcao_escolhida == 3:
-  print('Ativar restaurantes')
-else:
-  finalizar_app()
+def escolher_opcoes():
+  opcao_escolhida = int(input('Escolha uma opção: '))
+  if opcao_escolhida == 1:
+    print('Cadastrar restaurantes')
+  elif opcao_escolhida == 2:
+    print('Listar restaurantes')
+  elif opcao_escolhida == 3:
+    print('Ativar restaurantes')
+  else:
+    finalizar_app()
+
+def main():
+  exibir_nome_do_programa()
+  exibir_opcoes()
+  escolher_opcoes()
+
+if __name__ == '__main__':
+  main()
