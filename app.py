301fb317efc39305d3b9b477395757883e3d7278
luciliarosa <lucilia.passos.rosa@outlook.com>
2026-01-07 15:14:53 -0300

    fix: handle invalid menu input

--- app.py
+++ app.py
@@ -16,18 +16,29 @@ def finalizar_app():
   os.system('cls')
   print('Finalizando APP\n')
 
+def opcao_invalida():
+  print('Opção Inválida!\n')
+  input('Digite a telha ENTER para voltar ao MENU PRINCIPAL ')
+  main()
+
 def escolher_opcoes():
-  opcao_escolhida = int(input('Escolha uma opção: '))
-  if opcao_escolhida == 1:
-    print('Cadastrar restaurantes')
-  elif opcao_escolhida == 2:
-    print('Listar restaurantes')
-  elif opcao_escolhida == 3:
-    print('Ativar restaurantes')
-  else:
-    finalizar_app()
+  try:
+    opcao_escolhida = int(input('Escolha uma opção: '))
+    if opcao_escolhida == 1:
+      print('Cadastrar restaurantes')
+    elif opcao_escolhida == 2:
+      print('Listar restaurantes')
+    elif opcao_escolhida == 3:
+      print('Ativar restaurantes')
+    elif opcao_escolhida == 4:
+      finalizar_app()
+    else:
+      opcao_invalida()
+  except:
+    opcao_invalida()
 
 def main():
+  os.system('cls')
   exibir_nome_do_programa()
   exibir_opcoes()
   escolher_opcoes()
