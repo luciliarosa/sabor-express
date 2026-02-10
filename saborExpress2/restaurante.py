a8b02171a9412f3edf43b3bdecb6e739943c6fa1
luciliarosa <lucilia.passos.rosa@outlook.com>
2026-02-10 11:42:30 -0300

    feat: add Restaurant class with constructor and attributes

---
+++ saborExpress2/restaurante.py
@@ -0,0 +1,15 @@
+class Restaurante: 
+    def __init__(self, nome, categoria):
+        self.nome = nome
+        self.categoria = categoria
+        self.ativo = False
+
+    def __str__(self):
+        return f'{self.nome} | {self.categoria}'
+
+restaurante_praca = Restaurante('Praça', 'Gourmet')
+restaurante_pizza = Restaurante('Pizza Express', 'Italiana')
+
+restaurantes = [restaurante_praca, restaurante_pizza]
+
+print(restaurante_praca)
